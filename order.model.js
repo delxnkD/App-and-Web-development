74e36fcc83ade7dfcce7c366f9a70a6bd7467cc5
delxnkD <168845235+delxnkD@users.noreply.github.com>
2025-03-27 13:18:17 +0100

    Add files via upload

---
+++ order.model.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+
+const orderSchema = mongoose.Schema(
+    {
+        customer: {
+            fullName: { type: String, required: true },
+            email: { type: String, required: true },
+            address: { type: String, required: true },
+            city: { type: String, required: true },
+            zip: { type: String, required: true }
+        },
+        items: [
+            {
+                name: { type: String, required: true },
+                quantity: { type: Number, required: true },
+                price: { type: Number, required: true },
+                total: { type: Number, required: true },
+                image: { type: String, required: true }
+            }
+        ],
+        summary: {
+            subtotal: { type: Number, required: true },
+            tax: { type: Number, required: true },
+            total: { type: Number, required: true }
+        },
+        status: { type: String, default: 'Processing' }
+    },
+    { timestamps: true }
+);
+
+module.exports = mongoose.model('Order', orderSchema);
