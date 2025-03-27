8956aa5338efc89baba26d3b6528fbddadcdcf73
delxnkD <168845235+delxnkD@users.noreply.github.com>
2025-03-27 13:23:11 +0100

    Add files via upload

---
+++ product.routes.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+const { getProducts, getProduct, createProduct, updateProduct } = require('../controllers/product.controller');
+
+router.get('/', getProducts);
+router.get('/:id', getProduct);
+router.post('/', createProduct);
+router.put('/', updateProduct);
+
+module.exports = router;
